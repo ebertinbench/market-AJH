6d49185b3b1c75cf72b224bc40f9e7f075a5626c
ebertinbench <ebertinbench@debian>
2025-08-23 04:34:41 +0200

    ajout des fixtures en prod

--- market-ajh/config/bundles.php
+++ market-ajh/config/bundles.php
@@ -9,6 +9,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true, 'prod' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
