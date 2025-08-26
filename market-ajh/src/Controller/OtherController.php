851583c3c8d510a6822d3d1d15f0fe96f24c268f
ebertinbench <ebertinbench@debian>
2025-08-26 16:53:06 +0000

    ajout du mailer et d'un formulaire de contact

--- market-ajh/src/Controller/OtherController.php
+++ market-ajh/src/Controller/OtherController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 #[Route('/other')]
 final class OtherController extends AbstractController
@@ -24,5 +26,33 @@ final class OtherController extends AbstractController
             'controller_name' => 'OtherController',
             'nomdepage' => 'Conditions d\'utilisation'
         ]);
+    }
+
+    #[Route('/contact', name:'app_contact', methods:['GET'])]
+    public function contact(): Response
+    {
+        return $this->render('other/contact.html.twig', [
+            'controller_name' => 'OtherController',
+            'nomdepage' => 'Contact'
+        ]);
+    }
+    #[Route('/contact', name: 'app_contact_submit', methods: ['POST'])]
+    public function contactSubmit(MailerInterface $mailer): Response
+    {
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        
+        $subject = $request->request->get('subject');
+        $message = $request->request->get('message');
+        $email = (new Email())
+            ->from('your_email@example.com')
+            ->to('contact@market-ajh.fr')
+            ->subject($subject)
+            ->text($message);
+
+        $mailer->send($email);
+
+        $this->addFlash('success', 'Votre message a été envoyé avec succès!');
+        
+        return $this->redirectToRoute('app_contact');
     }
 }
